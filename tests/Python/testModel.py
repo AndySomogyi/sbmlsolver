9bb3a9fae5002a0fa2bb927f33c8af1383036aa4
hsauro <hsauro@c0027639-9190-0b57-01eb-f09a03790dae>
2012-12-31 03:41:03 +0000

    First commit of a test model
    git-svn-id: https://roadrunnerlib.googlecode.com/svn/trunk@466 c0027639-9190-0b57-01eb-f09a03790dae

---
+++ tests/Python/testModel.py
@@ -0,0 +1,11 @@
+
+import sys
+import rrTester
+
+pathToModels = 'C:\\vs\\trunk\\tests\\'
+
+# runTester takes two arguments:
+#  1) The path to the results and model file
+#  2) The name of the results and model file
+
+rrTester.runTester (pathToModels, 'results_roadRunnerTest_1.txt')
