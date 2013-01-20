19b63c1ca0699f9c678f8e2ecbbafa10d51d655f
tottek@gmail.com <tottek@gmail.com@c0027639-9190-0b57-01eb-f09a03790dae>
2013-01-20 03:57:17 +0000

    Fixed CPACK problem for dll and libs Added new component category to installer: testing
    Updated the installer with the latest
    
    git-svn-id: https://roadrunnerlib.googlecode.com/svn/trunk@544 c0027639-9190-0b57-01eb-f09a03790dae

--- ThirdParty/libsbml/src/sbml/common/libsbml-config-common.h
+++ ThirdParty/libsbml/src/sbml/common/libsbml-config-common.h
@@ -63,7 +63,7 @@
 #define HAVE_ERRNO_H 1 
 
 /* Define to 1 if you have the <ieeefp.h> header file. */
-/* #undef HAVE_IEEEFP_H */
+#define HAVE_IEEEFP_H 1
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -75,7 +75,7 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the `m' library (-lm). */
-#define HAVE_LIBM 1
+/* #undef HAVE_LIBM */
 
 
 /* Define to 1 to enable primitive memory tracing. */
