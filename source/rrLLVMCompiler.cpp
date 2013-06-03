6b4bf8adafb8c675e3e41d7f3dd618c896a85981
andy.somogyi@gmail.com <andy.somogyi@gmail.com@c0027639-9190-0b57-01eb-f09a03790dae>
2013-06-03 16:06:24 +0000

    initial stubbed out LLVM classes
    git-svn-id: https://roadrunnerlib.googlecode.com/svn/branches/feature-llvm@1008 c0027639-9190-0b57-01eb-f09a03790dae

---
+++ source/rrLLVMCompiler.cpp
@@ -0,0 +1,25 @@
+/*
+ * rrLLVMCompiler.cpp
+ *
+ *  Created on: Jun 3, 2013
+ *      Author: andy
+ */
+
+#include "rrLLVMCompiler.h"
+
+namespace rr
+{
+
+LLVMCompiler::LLVMCompiler() :
+        Compiler("", "")
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+LLVMCompiler::~LLVMCompiler()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace rr */
