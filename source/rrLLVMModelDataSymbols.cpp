fcec289585075b882b228f23d8ccdad0899fb719
andy.somogyi@gmail.com <andy.somogyi@gmail.com@c0027639-9190-0b57-01eb-f09a03790dae>
2013-06-18 20:29:26 +0000

    new llvm files
    git-svn-id: https://roadrunnerlib.googlecode.com/svn/branches/develop@1129 c0027639-9190-0b57-01eb-f09a03790dae

---
+++ source/rrLLVMModelDataSymbols.cpp
@@ -0,0 +1,27 @@
+/*
+ * rrLLVMModelDataSymbols.cpp
+ *
+ *  Created on: Jun 17, 2013
+ *
+ * Author: Andy Somogyi,
+ *     email decode: V1 = "."; V2 = "@"; V3 = V1;
+ *     andy V1 somogyi V2 gmail V3 com
+ */
+
+#include "rrLLVMModelDataSymbols.h"
+
+namespace rr
+{
+
+LLVMModelDataSymbols::LLVMModelDataSymbols()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+LLVMModelDataSymbols::~LLVMModelDataSymbols()
+{
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace rr */
