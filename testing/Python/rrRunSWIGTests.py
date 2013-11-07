96681a93556d3354fbb1055eb430f0d9a774ad61
hsauro <hsauro@u.washington.edu>
2013-11-06 22:27:38 -0800

    Update rrRunSWIGTests.py

--- testing/Python/rrRunSWIGTests.py
+++ testing/Python/rrRunSWIGTests.py
@@ -1,12 +1,11 @@
 import sys
-import rrTester
-import rrPython
+import rrSWIGTests
 import numpy
 
-pathToModels = 'C:\\vs\\trunk\\Wrappers\\Python\\Testing\\'
-#pathToModels = 'R:\\installs\\vs_2012\\RelWDebInfo\\testing\\'
+pathToModels = 'C:\\Python27\\Lib\\roadrunner\\testing\\Python\\'
+
 # runTester takes two arguments:
 #  1) The path to the results and model file
 #  2) The name of the results and model file
 
-rrTester.runTester (pathToModels, 'results_roadRunnerTest_1.txt')
+rrSWIGTests.runTester (pathToModels, 'results_roadRunnerTest_1.txt')
