197f6f3582401bc466dfcff22322993226c5b01b
Totte Karlsson <tottek@gmail.com>
2013-11-18 15:46:35 -0800

    Adding docs for levenberg marquardt plugin

---
+++ plugins/lmfit/docs/lmfit_doc.h
@@ -0,0 +1,5 @@
+#ifndef lmfit_docH
+#define lmfit_docH
+RR_PLUGIN_DECLSPEC extern unsigned char pdf_doc[];
+RR_PLUGIN_DECLSPEC extern size_t sizeofPDF;
+#endif
--- plugins/lmfit/docs/makeDocs.sh
+++ plugins/lmfit/docs/makeDocs.sh
@@ -1,9 +1,27 @@
 #! /usr/bin/bash
 
-echo "Creating documentation for your plugin"
+echo "Creating documentation for the plugin"
 pluginName=lmfit
 chapter=plugin_$pluginName
-mainDoc=main.tex
+mainDoc=$pluginName.tex
 pdflatex -jobname=$pluginName "\includeonly{$chapter}\input{$mainDoc}"
+
+#Create the C file to link with the plugin
+hdrFile="$pluginName"_doc.h
+cppFile="$pluginName"_doc.cpp  
+
+#Create header
+echo "#ifndef $pluginName"_docH > $hdrFile
+echo "#define $pluginName"_docH >> $hdrFile
+
+echo "RR_PLUGIN_DECLSPEC extern unsigned char pdf_doc[];" >> $hdrFile
+echo "RR_PLUGIN_DECLSPEC extern size_t sizeofPDF;" >> $hdrFile
+echo "#endif" >> $hdrFile
+
+#create source
+cat $pluginName.pdf | (echo "unsigned char pdf_doc[]={";xxd -i; echo "};") > $cppFile
+echo "int sizeofPDF = sizeof(pdf_doc);" >> $cppFile
+
+echo "Done.."
 
 
