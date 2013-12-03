d6a629a01ad19709a1ddb72e2d18e54abd268e2e
Totte Karlsson <tottek@gmail.com>
2013-12-03 13:47:11 -0800

    Update

--- plugins/docs/C/html/search/all_69.js
+++ plugins/docs/C/html/search/all_69.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['invalid_5fhandle_5ferror_5fmsg',['INVALID_HANDLE_ERROR_MSG',['../rrp__utilities_8h.html#af8776c486d2155e2b40d97013c89c558',1,'rrp_utilities.h']]],
-  ['isbeingterminated',['isBeingTerminated',['../group__plugins.html#ga59dc04358e6889a41a1e2d5eb9d0aaff',1,'rrp::isBeingTerminated()'],['../group__plugins.html#ga59dc04358e6889a41a1e2d5eb9d0aaff',1,'isBeingTerminated():&#160;rrp_api.cpp']]],
-  ['ispluginworking',['isPluginWorking',['../group__plugins.html#gab28dca1a69d8e779fc45bb754ef68982',1,'rrp::isPluginWorking()'],['../group__plugins.html#gab28dca1a69d8e779fc45bb754ef68982',1,'isPluginWorking():&#160;rrp_api.cpp']]]
+  ['isbeingterminated',['isBeingTerminated',['../group__plugins.html#gaa2a6a48712bc7152f5e1e8a59bc37e24',1,'rrp::isBeingTerminated()'],['../group__plugins.html#gaa2a6a48712bc7152f5e1e8a59bc37e24',1,'isBeingTerminated():&#160;rrp_api.cpp']]],
+  ['ispluginworking',['isPluginWorking',['../group__plugins.html#ga111e8e21b2869b5d4b2a51040b957fb4',1,'rrp::isPluginWorking()'],['../group__plugins.html#ga111e8e21b2869b5d4b2a51040b957fb4',1,'isPluginWorking():&#160;rrp_api.cpp']]]
 ];
