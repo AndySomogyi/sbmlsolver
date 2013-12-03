fd66874cf4638a7ae5141989001614eab4ea6421
Totte Karlsson <tottek@gmail.com>
2013-12-03 12:30:54 -0800

    Added initial C++ docs for plugins

--- plugins/docs/C/html/search/all_72.js
+++ plugins/docs/C/html/search/all_72.js
@@ -3,6 +3,7 @@ var searchData=
   ['resetplugin',['resetPlugin',['../group__plugins.html#gad23cafc934134288ef49f5834d05880f',1,'rrp::resetPlugin()'],['../group__plugins.html#gad23cafc934134288ef49f5834d05880f',1,'resetPlugin():&#160;rrp_api.cpp']]],
   ['rrp_5fapi_2eh',['rrp_api.h',['../rrp__api_8h.html',1,'']]],
   ['rrp_5fcpp_5fsupport_2eh',['rrp_cpp_support.h',['../rrp__cpp__support_8h.html',1,'']]],
+  ['rrp_5fexporter_2eh',['rrp_exporter.h',['../rrp__exporter_8h.html',1,'']]],
   ['rrp_5fparameter_5fapi_2eh',['rrp_parameter_api.h',['../rrp__parameter__api_8h.html',1,'']]],
   ['rrp_5ftypes_2eh',['rrp_types.h',['../rrp__types_8h.html',1,'']]],
   ['rrp_5futilities_2eh',['rrp_utilities.h',['../rrp__utilities_8h.html',1,'']]],
