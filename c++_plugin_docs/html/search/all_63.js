46741d459239bc976545cbcf9f9a4444fe19c7d8
Michal Galdzicki <mgaldzic@gmail.com>
2013-12-04 16:24:31 -0800

    new c++_plugin_docs

---
+++ c++_plugin_docs/html/search/all_63.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['capabilities',['Capabilities',['../classrrp_1_1_capabilities.html',1,'rrp']]],
+  ['capability',['Capability',['../classrrp_1_1_capability.html',1,'rrp']]],
+  ['cplugin',['CPlugin',['../classrrp_1_1_c_plugin.html',1,'rrp']]],
+  ['cppplugin',['CPPPlugin',['../classrrp_1_1_c_p_p_plugin.html',1,'rrp']]]
+];
---
+++ c++_plugin_docs/html/search/functions_77.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['wasterminated',['wasTerminated',['../classrrp_1_1_plugin.html#ad1aa634241ef70a0b4af580f21b8ac16',1,'rrp::Plugin']]]
+];
