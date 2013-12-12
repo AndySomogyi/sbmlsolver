4432188305b61969cdde858e9bc58142d084fb0f
Andy Somogyi <andy.somogyi@gmail.com>
2013-12-11 19:56:30 -0500

    added doc copy script

---
+++ copy_docs_to_rr.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp -r c++_plugin_api_docs $1/
+cp -r c_plugin_api_docs   $1/
+cp index.html             $1/
+cp -r c_api_docs          $1/ 
+cp -r cpp_api_docs        $1/
+cp -r python_docs         $1/
