b2efa2657abb5f3db4ab12b6145826d5eb70b816
Andy Somogyi <andy.somogyi@gmail.com>
2013-12-15 14:57:45 -0500

    updates

--- rrLibPluginsPythonAPI/html/search/all_63.js
+++ rrLibPluginsPythonAPI/html/search/all_63.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['createparameter',['createParameter',['../group__plugin__parameters.html#ga935eb24b2c15b6341ac2ef9f6136a6f7',1,'rrPlugins']]],
-  ['createpluginmanager',['createPluginManager',['../group__plugin__manager.html#ga49235bbd20c3542fa55135c8db73de1c',1,'rrPlugins']]]
+  ['createpluginmanager',['createPluginManager',['../group__plugin__manager.html#ga49235bbd20c3542fa55135c8db73de1c',1,'rrPlugins']]],
+  ['createrrcdata',['createRRCData',['../group__utilities.html#ga4f8d56c3d083a83bef7c5a658f1fe206',1,'rrPlugins']]]
 ];
