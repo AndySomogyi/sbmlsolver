70b265fe539aa6b053af4d633daddaa91ef6cda5
Andy Somogyi <andy.somogyi@gmail.com>
2014-01-22 15:08:01 -0500

    plugins moved to Tellurium

--- plugins/add_noise/add_noise_docs.h
+++
@@ -1,7 +0,0 @@
-#ifndef add_noise_docsH
-#define add_noise_docsH
-#include <stddef.h>
-extern unsigned char pdf_doc[];
-extern size_t sizeofPDF;
-
-#endif
--- plugins/add_noise/docs/makeDocs.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-echo "Creating documentation for your plugin"
-pluginName=add_noise
-chapter=plugin_$pluginName
-mainDoc=main.tex
-pdflatex -jobname=$pluginName "\includeonly{$chapter}\input{$mainDoc}"
-
-
