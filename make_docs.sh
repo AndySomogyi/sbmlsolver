4c6cd49acb6ee3006b38fbc151801fbe5e45ec35
Andy Somogyi <andy.somogyi@gmail.com>
2014-07-18 17:24:04 -0400

    update make_docs script to deal with directory with spaces

--- make_docs.sh
+++ make_docs.sh
@@ -4,18 +4,21 @@
 # this 'script' is not intended to live any longer than a 
 # week or so utill we get a system down to make docs.
 
-cwd=`pwd`
+cwd=""`pwd`""
 
-cd source 
+echo "cwd is ${cwd}"
+
+
+cd "$cwd/source"
 doxygen doxygen.conf
-cd $cwd
+cd "$cwd"
 
 
-cd wrappers/C
+cd "$cwd/wrappers/C"
 doxygen core_c_api.doxy
-cd $cwd
+cd "$cwd"
 
-cd  wrappers/Python/doc
+cd  "$cwd/wrappers/Python/doc"
 make html
-cd $cwd
+cd "$cwd"
 
