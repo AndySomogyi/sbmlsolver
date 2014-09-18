d959b71ea7ca7e7f9ed58faecd06afa896a5fad5
Andy Somogyi <andy.somogyi@gmail.com>
2014-09-18 13:59:33 -0400

    use standard sbml consistency check

--- source/SBMLValidator.cpp
+++ source/SBMLValidator.cpp
@@ -9,110 +9,60 @@
 #include "rrSBMLReader.h"
 
 #include <sbml/SBMLDocument.h>
-#include <sbml/SBMLReader.h>
-
-#include <sbml/validator/ConsistencyValidator.h>
-#include <sbml/validator/UnitConsistencyValidator.h>
-#include <sbml/validator/IdentifierConsistencyValidator.h>
-#include <sbml/validator/MathMLConsistencyValidator.h>
-#include <sbml/validator/OverdeterminedValidator.h>
-#include <sbml/validator/ModelingPracticeValidator.h>
 
 #include "rrLogger.h"
 
 #include <sstream>
 
-using libsbml::SBMLDocument;
-using libsbml::readSBMLFromString;
-using std::string;
-
 using namespace libsbml;
 
-
-typedef std::list<SBMLError> SBMLErrorList;
-
-
+using std::string;
 
 namespace rr
 {
 
-struct RRValidator
-{
-    std::stringstream errors;
-    SBMLDocument *doc;
-
-    RRValidator() : doc(NULL) {}
-
-    void load(const std::string& src) {
-        string sbml = SBMLReader::read(src);
-        doc =  readSBMLFromString (sbml.c_str());
-    }
-
-    ~RRValidator() {
-        delete doc;
-    }
-
-    void validate(libsbml::Validator& val, const std::string& name) {
-        val.init();
-
-        int err = val.validate(*doc);
-
-        if (err > 0) {
-
-            errors << name << ":" << std::endl;
-
-            for (SBMLErrorList::const_iterator i = val.getFailures().begin();
-                    i != val.getFailures().end(); ++i) {
-                i->print(errors);
-            }
-        }
-    }
-};
-
-
 std::string validateSBML(const std::string src, unsigned opt)
 {
-    RRValidator v;
+    std::stringstream errors;
+    SBMLDocument *doc = NULL;
 
-    v.load(src);
+    try {
+        string sbml = SBMLReader::read(src);
+        doc =  readSBMLFromString (sbml.c_str());
 
-    if (opt & VALIDATE_GENERAL)
-    {
-        ConsistencyValidator val;
-        v.validate(val, "VALIDATE_GENERAL");
+        doc->setConsistencyChecks(LIBSBML_CAT_GENERAL_CONSISTENCY,
+                opt & VALIDATE_GENERAL);
+
+        doc->setConsistencyChecks(LIBSBML_CAT_UNITS_CONSISTENCY,
+                opt & VALIDATE_UNITS);
+
+        doc->setConsistencyChecks(LIBSBML_CAT_IDENTIFIER_CONSISTENCY,
+                opt & VALIDATE_IDENTIFIER);
+
+        doc->setConsistencyChecks(LIBSBML_CAT_MATHML_CONSISTENCY,
+                opt & VALIDATE_MATHML);
+
+        doc->setConsistencyChecks(LIBSBML_CAT_OVERDETERMINED_MODEL,
+                opt & VALIDATE_OVERDETERMINED);
+
+        doc->setConsistencyChecks(LIBSBML_CAT_MODELING_PRACTICE,
+                opt & VALIDATE_MODELING_PRACTICE);
+
+        doc->checkConsistency();
+
+        SBMLErrorLog* errorLog = doc->getErrorLog();
+
+        if(errorLog) {
+            errorLog->printErrors(errors);
+        }
+
+    } catch(...) {
+        delete doc;
+        throw;
     }
 
-    if (opt & VALIDATE_UNITS)
-    {
-        UnitConsistencyValidator val;
-        v.validate(val, "VALIDATE_UNITS");
-    }
-
-    if (opt & VALIDATE_IDENTIFIER)
-    {
-        IdentifierConsistencyValidator val;
-        v.validate(val, "VALIDATE_IDENTIFIER");
-    }
-
-    if (opt & VALIDATE_MATHML)
-    {
-        MathMLConsistencyValidator val;
-        v.validate(val, "VALIDATE_MATHML");
-    }
-
-    if (opt & VALIDATE_OVERDETERMINED)
-    {
-        OverdeterminedValidator val;
-        v.validate(val, "VALIDATE_OVERDETERMINED");
-    }
-
-    if (opt & VALIDATE_MODELING_PRACTICE)
-    {
-        ModelingPracticeValidator val;
-        v.validate(val, "VALIDATE_MODELING_PRACTICE");
-    }
-
-    return v.errors.str();
+    delete doc;
+    return errors.str();
 }
 
 } /* namespace rr */
