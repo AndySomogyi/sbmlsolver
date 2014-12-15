aa402a7804967be8e8975e4fab3550ff538fc576
0u812 <0u812@github.com>
2014-12-15 12:55:49 -0800

    add tr1 proxies for memory and unordered_map

---
+++ source/tr1proxy/rr_unordered_map.h
@@ -0,0 +1,30 @@
+// == PREAMBLE ================================================
+
+// * Licensed under the Apache License, Version 2.0; see README
+
+// == FILEDOC =================================================
+
+/** @file rr_unordered_map.h
+  * @author JKM
+  * @date 12/15/2014
+  * @copyright Apache License, Version 2.0
+  * @brief A proxy for maintaining compatibility across C++ std library versions
+  * @details C++11 moved tr1 headers and classes into the standard namespace,
+  * hence no use for a tr1 prefix.
+**/
+
+# if __APPLE__
+  // is Mavericks
+  # if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_9
+    # include <unordered_map>
+  # else
+    # include <tr1/unordered_map>
+  # endif                                                   // OSX ver
+# else                                                      // not __APPLE__
+  # if (__cplusplus >= 201103L) || defined(_MSC_VER)
+    # include <unordered_map>
+  # else
+    # include <tr1/unordered_map>
+  # endif
+# endif                                                     // __APPLE__
+
