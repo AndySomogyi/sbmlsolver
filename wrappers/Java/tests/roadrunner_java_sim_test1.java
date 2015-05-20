d56465d2933ac1ec279ce24d78d7987f73d7975f
0u812 <0u812@github.com>
2015-05-20 10:39:21 -0700

    switch model

--- wrappers/Java/tests/roadrunner_java_sim_test1.java
+++ wrappers/Java/tests/roadrunner_java_sim_test1.java
@@ -3,7 +3,7 @@ import roadrunner.*;
 
 public class roadrunner_java_sim_test1 {
   public static void main(String argv[]) {
-    RoadRunner rr = new RoadRunner("/home/poltergeist/devel/models/GlycolysisOriginal.xml");
+    RoadRunner rr = new RoadRunner("/home/poltergeist/devel/models/decayModel.xml");
     System.out.println(rr.getExtendedVersionInfo());
     System.out.println("Loaded rr");
     rr.simulate();
