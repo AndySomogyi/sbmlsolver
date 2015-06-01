3fc59d1f759b0caf7dbf239c534fd309654eba71
user <user@ubuntu-12.04-kvm>
2015-06-01 13:59:04 -0700

    add generated debian pkg files

---
+++ packaging/debian/libroadrunner.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libroadrunner initscript
+# sourced by /etc/init.d/libroadrunner
+# installed at /etc/default/libroadrunner by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ packaging/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libroadrunner):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libroadrunner" command="/usr/bin/libroadrunner"
