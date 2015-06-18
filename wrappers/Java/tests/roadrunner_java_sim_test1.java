c4ba5255e7b95fe4314862ca8944c57f77567db6
0u812 <0u812@github.com>
2015-06-18 11:05:53 -0700

    output formatting

--- wrappers/Java/tests/roadrunner_java_sim_test1.java
+++ wrappers/Java/tests/roadrunner_java_sim_test1.java
@@ -7,8 +7,9 @@ public class roadrunner_java_sim_test1 {
   public static void main(String argv[]) {
     RoadRunner rr = new RoadRunner("/home/poltergeist/devel/models/decayModel.xml");
     System.out.println(rr.getExtendedVersionInfo());
-    System.out.println("Loaded rr");
+    System.out.println("- Loaded rr");
     Matrix x = rr.simulate();
-    System.out.println("simulate called");
+    System.out.println("- Simulate called");
+    System.out.println(x);
   }
 }
--- wrappers/Java/tests/roadrunner_javajni_test1.java
+++ wrappers/Java/tests/roadrunner_javajni_test1.java
@@ -5,6 +5,6 @@ public class roadrunner_javajni_test1 {
   public static void main(String argv[]) {
     RoadRunner rr = new RoadRunner();
     System.out.println(rr.getExtendedVersionInfo());
-    System.out.println("Loaded rr");
+    System.out.println("- Loaded rr");
   }
 }
