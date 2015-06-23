f110cfbed4868155d458082f0be17f895e0270ff
0u812 <0u812@github.com>
2015-06-23 12:19:20 -0700

    use minimal decay model

--- wrappers/Java/tests/roadrunner_java_sim_test1.java
+++ wrappers/Java/tests/roadrunner_java_sim_test1.java
@@ -5,7 +5,7 @@ import org.la4j.Matrix;
 
 public class roadrunner_java_sim_test1 {
   public static void main(String argv[]) {
-    RoadRunner rr = new RoadRunner("/home/poltergeist/devel/models/decayModel.xml");
+    RoadRunner rr = new RoadRunner("/home/poltergeist/devel/models/decayModelMinimal.xml");
     System.out.println(rr.getExtendedVersionInfo());
     System.out.println("- Loaded rr");
     LabeledData x = rr.simulate(0, 10, 100);
