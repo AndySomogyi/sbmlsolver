569099cda4f1037e05a1de97e7f2a26fb6e81f53
0u812 <0u812@users.noreply.github.com>
2015-07-31 11:13:06 -0700

    add docs to cvode integrator

--- source/CVODEIntegrator.h
+++ source/CVODEIntegrator.h
@@ -14,72 +14,136 @@ typedef struct _generic_N_Vector *N_Vector;
 
 namespace rr
 {
+    using std::string;
 
-	using std::string;
+    class ExecutableModel;
+    class RoadRunner;
 
-	class ExecutableModel;
-	class RoadRunner;
+    /**
+     * @author WBC, ETS, MTK
+     * @brief A RoadRunner integrator based on CVODE; serves as RoadRunner's main integrator for ODEs
+     * @details Provides a wrapper around CVODE from the SUNDIALS suite.
+     * CVODE has two main solvers: an Adams-Moulton solver for non-stiff problems,
+     * and a backward differentiation formula (BDF) solver for stiff problems.
+     * See: https://computation.llnl.gov/casc/sundials/documentation/toms_sundials.pdf
+     */
+    class CVODEIntegrator : public Integrator
+    {
 
-	/**
-	* @internal
-	* The integrator implemented by CVODE.
-	*/
-	class CVODEIntegrator : public Integrator
-	{
+    public:
+        CVODEIntegrator(ExecutableModel* oModel);
+        virtual ~CVODEIntegrator();
 
-	public:
-		CVODEIntegrator(ExecutableModel* oModel);
-		virtual ~CVODEIntegrator();
-		
-		void loadConfigSettings();
-		void loadSBMLSettings(const std::string& filename);
-		std::string getIntegratorName() const;
-		std::string getIntegratorDescription() const;
-		std::string getIntegratorHint() const;
-		IntegrationMethod getIntegrationMethod() const;
-		void setValue(std::string setting, const Variant& value);
-		
-		double integrate(double t0, double tf);
-		void restart(double timeStart);
-		void tweakTolerances();
-		IntegratorListenerPtr getListener();
-		void setListener(IntegratorListenerPtr);
+        void loadConfigSettings();
 
-	private:
-		static const int mDefaultMaxNumSteps;
-		static const int mDefaultMaxAdamsOrder;
-		static const int mDefaultMaxBDFOrder;
+        /**
+         * @author WBC
+         * @brief Load an SBML settings file and apply the configuration options
+         * @details
+         */
+        void loadSBMLSettings(const std::string& filename);
 
-		void* mCVODE_Memory;
-		N_Vector mStateVector;
-		ExecutableModel* mModel;
+        /**
+         * @author WBC
+         * @brief Get the name for this integrator
+         */
+        std::string getIntegratorName() const;
 
-		IntegratorListenerPtr listener;
-		double lastEventTime;
-		bool variableStepPendingEvent;
-		bool variableStepTimeEndEvent;
-		double *variableStepPostEventState;
-		std::vector<unsigned char> eventStatus;
+        /**
+         * @author WBC
+         * @brief Get the description for this integrator
+         */
+        std::string getIntegratorDescription() const;
 
-		void testRootsAtInitialTime();
-		bool haveVariables();
-		void assignResultsToModel();
-		void setCVODETolerances();
-		void reInit(double t0);
+        /**
+         * @author WBC
+         * @brief Get the hint for this integrator
+         */
+        std::string getIntegratorHint() const;
 
-		void updateCVODE();
-		void applyPendingEvents(double timeEnd);
-		void applyEvents(double timeEnd, std::vector<unsigned char> &previousEventStatus);
-		double applyVariableStepPendingEvents();
+        /**
+         * @author WBC, ETS, MTK
+         * @brief Always deterministic for CVODE
+         */
+        IntegrationMethod getIntegrationMethod() const;
 
-		void createCVode();
-		void freeCVode();
-		bool stateVectorVariables;
+        /**
+         * @author WBC, ETS, MTK
+         * @brief Sets the value of an integrator setting (e.g. absolute_tolerance)
+         */
+        void setValue(std::string setting, const Variant& value);
+
+        /**
+         * @author WBC, ETS, MTK
+         * @brief Main integration routine
+         */
+        double integrate(double t0, double tf);
+
+        /**
+         * @author WBC, ETS, MTK
+         * @brief Reset time to zero and reinitialize model
+         * @details Applies events which occur before time zero.
+         * Reinitializes CVODE and the executable model.
+         */
+        void restart(double timeStart);
+
+        /**
+         * @author WBC, ETS, MTK
+         * @brief Sets tolerances based on config values
+         * @details Sets absolute and relative tolerances to the minimum
+         * of Config::CVODE_MIN_ABSOLUTE, Config::CVODE_MIN_RELATIVE and
+         * their respective current values
+         */
+        void tweakTolerances();
 
 
-		friend int cvodeDyDtFcn(double t, N_Vector cv_y, N_Vector cv_ydot, void *f_data);
-		friend int cvodeRootFcn(double t, N_Vector y, double *gout, void *g_data);
-	};
+        /**
+         * @author WBC
+         * @brief Gets the integrator listener
+         */
+        IntegratorListenerPtr getListener();
+
+        /**
+         * @author WBC
+         * @brief Sets the integrator listener
+         */
+        void setListener(IntegratorListenerPtr);
+
+    private:
+        static const int mDefaultMaxNumSteps;
+        static const int mDefaultMaxAdamsOrder;
+        static const int mDefaultMaxBDFOrder;
+
+        void* mCVODE_Memory;
+        N_Vector mStateVector;
+        ExecutableModel* mModel;
+
+        IntegratorListenerPtr listener;
+        double lastEventTime;
+        bool variableStepPendingEvent;
+        bool variableStepTimeEndEvent;
+        double *variableStepPostEventState;
+        std::vector<unsigned char> eventStatus;
+
+        void testRootsAtInitialTime();
+        bool haveVariables();
+        void assignResultsToModel();
+        void setCVODETolerances();
+        void reInit(double t0);
+
+        void updateCVODE();
+        void applyPendingEvents(double timeEnd);
+        void applyEvents(double timeEnd, std::vector<unsigned char> &previousEventStatus);
+        double applyVariableStepPendingEvents();
+
+        void createCVode();
+        void freeCVode();
+        bool stateVectorVariables;
+
+
+        friend int cvodeDyDtFcn(double t, N_Vector cv_y, N_Vector cv_ydot, void *f_data);
+        friend int cvodeRootFcn(double t, N_Vector y, double *gout, void *g_data);
+    };
 }
 
 #endif
