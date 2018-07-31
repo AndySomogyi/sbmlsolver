637e09e7a0001879d6e6ce56dff7f459cd711ad7
Andrew Hu <andrewhu@uw.edu>
2018-07-31 15:55:03 -0700

    +subdivision_steps setting

--- source/EulerIntegrator.h
+++ source/EulerIntegrator.h
@@ -75,10 +75,12 @@ namespace rr
         EulerIntegrator(ExecutableModel *m)
 			:	eventStatus(std::vector<unsigned char>(m->getNumEvents(),false)),
 				previousEventStatus(std::vector<unsigned char>(m->getNumEvents(), false)) {
+            resetSettings();
+
             model = m;
             exampleParameter1 = 3.14;
             exampleParameter2 = "hello";
-			Log(rr::Logger::LOG_WARNING) << "Euler integrator is inaccurate";
+			Log(Logger::LOG_WARNING) << "Euler integrator is inaccurate";
 			//std::cerr << "Number of event triggers: " << m->getEventTriggers(0, 0, 0) << std::endl;
 
             if(model) {
@@ -114,58 +116,66 @@ namespace rr
         * @return the end time.
         */
         virtual double integrate(double t0, double h) {
+			int internal_steps = getValueAsInt("subdivision_steps");
 			if (model == (rr::ExecutableModel*)NULL) return 0;
 
-            // evaluate and copy the rate of change of the state vector
-            // rate into the local buffer. If the 2nd argument is NULL,
-            // the current model state is used to evaluate the
-            // state vector rate.
-            model->getStateVectorRate(t0, NULL, rateBuffer);
+            double finalTimeEnd;
 
-            // copy the current state vector into a local buffer
-            model->getStateVector(stateBufferBegin);
+			h /= internal_steps;
+			for (int subdiv = 0; subdiv < internal_steps; ++subdiv) {
+				// evaluate and copy the rate of change of the state vector
+				// rate into the local buffer. If the 2nd argument is NULL,
+				// the current model state is used to evaluate the
+				// state vector rate.
+				model->getStateVectorRate(t0, NULL, rateBuffer);
 
-            // perform the Euler integration step, i.e.
-            // y_{n+1} = y_{n} + h * y'_{n}
-            for (int i = 0; i < stateVectorSize; ++i) {
-                stateBufferEnd[i] = stateBufferBegin[i] + h * rateBuffer[i];
-            }
+				// copy the current state vector into a local buffer
+				model->getStateVector(stateBufferBegin);
 
-            // set the model state to the newly calculated state
-            model->setStateVector(stateBufferEnd);
-
-            // update the model time to the new time
-			double timeEnd = t0 + h;
-            model->setTime(timeEnd);
-
-            // if we have a client, notify them that we have taken
-            // a time step
-            if (listener) {
-                listener->onTimeStep(this, model, timeEnd);
-            }
-
-			// events
-			bool triggered = false;
-
-			model->getEventTriggers(eventStatus.size(), NULL, eventStatus.size() ? &eventStatus[0] : NULL);
-			for (int k_ = 0; k_<eventStatus.size(); ++k_) {
-				if (eventStatus.at(k_)) {
-					triggered = true;
-					//std::cerr << "Triggered" << std::endl;
+				// perform the Euler integration step, i.e.
+				// y_{n+1} = y_{n} + h * y'_{n}
+				for (int i = 0; i < stateVectorSize; ++i) {
+					stateBufferEnd[i] = stateBufferBegin[i] + h * rateBuffer[i];
 				}
+
+				// set the model state to the newly calculated state
+				model->setStateVector(stateBufferEnd);
+
+				// update the model time to the new time
+				double timeEnd = t0 + h;
+				model->setTime(timeEnd);
+
+				// if we have a client, notify them that we have taken
+				// a time step
+				if (listener) {
+					listener->onTimeStep(this, model, timeEnd);
+				}
+
+				// events
+				bool triggered = false;
+
+				model->getEventTriggers(eventStatus.size(), NULL, eventStatus.size() ? &eventStatus[0] : NULL);
+				for (int k_ = 0; k_ < eventStatus.size(); ++k_) {
+					if (eventStatus.at(k_)) {
+						triggered = true;
+						//std::cerr << "Triggered" << std::endl;
+					}
+				}
+
+				if (triggered) {
+					// applyEvents takes the list of events which were previously triggered
+					//std::cerr << "An event was triggered at " << t0 << std::endl;
+					applyEvents(timeEnd, previousEventStatus);
+				}
+
+				if (eventStatus.size()) {
+					previousEventStatus = eventStatus;
+				}
+
+                finalTimeEnd = timeEnd;
 			}
 
-			if (triggered) {
-				// applyEvents takes the list of events which were previously triggered
-				//std::cerr << "An event was triggered at " << t0 << std::endl;
-				applyEvents(timeEnd, previousEventStatus);
-			}
-
-			if (eventStatus.size()) {
-				previousEventStatus = eventStatus;
-			}
-
-            return timeEnd;
+            return finalTimeEnd;
         }
 
 		void applyEvents(double timeEnd, std::vector<unsigned char> &previousEventStatus) {
@@ -276,15 +286,6 @@ namespace rr
 
         /**
          * @author JKM
-         * @brief Reset all integrator settings to their respective default values
-         */
-        void resetSettings() {
-            Solver::resetSettings();
-            // Euler integrator has no settings
-        }
-
-        /**
-         * @author JKM
          * @brief Always deterministic for Euler
          */
         IntegrationMethod getIntegrationMethod() const {
@@ -362,33 +363,14 @@ namespace rr
             return keys;
         }
 
-        /**
-        * list of keys that this integrator supports.
-        *
-        * This method is called by the IntegratorFactory to build a list of
-        * all the options that all the integrators support.
-        */
-        static const Dictionary* getIntegratorOptions() {
-            static BasicDictionary dict;
+		void resetSettings() {
+			Solver::resetSettings();
 
-            // fill out the required options for defining this integrator
-            dict.setItem("integrator", "euler");
-            dict.setItem("integrator.description",
-                    "Forward Euler Integrator: an example of creating an integrator");
-            dict.setItem("integrator.hint", "Forward Euler Integrator");
-
-            // this uses two example parameters, add them here
-            dict.setItem("exampleParameter1", 3.14);
-            dict.setItem("exampleParameter1.description",
-                    "an example parameter that does nothing");
-            dict.setItem("exampleParameter1.hint", "a hint for the example parameter");
-
-            dict.setItem("exampleParameter2", string("hello"));
-            dict.setItem("exampleParameter2.description",
-                    "another parameter which does nothing");
-            dict.setItem("exampleParameter2.hint", "a hint for the other parameter");
-
-            return &dict;
+			// Set default integrator settings.
+            addSetting("subdivision_steps", 1,
+                "Subdivision Steps",
+                "The number of subdivisions of the Euler step size (int).",
+                "(int) For each point, up to this many extra steps will be taken as smaller steps within each step, although their values are not saved");
         }
 
     private:
