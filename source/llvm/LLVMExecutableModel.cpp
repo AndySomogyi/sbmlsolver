df48e8eb3be4585f4f24e90119bbb6790e242212
Kiri Choi <kirichoi@uw.edu>
2018-09-24 16:21:04 -0700

    Remove debugging log

--- source/llvm/LLVMExecutableModel.cpp
+++ source/llvm/LLVMExecutableModel.cpp
@@ -138,10 +138,6 @@ int LLVMExecutableModel::setValues(bool (*funcPtr)(LLVMModelData*, int, double),
     {
         int j = indx ? indx[i] : i;
         bool result =  funcPtr(modelData, j, values[i]);
-        {
-          string id = (this->*getNameFuncPtr)(j);
-          Log(Logger::LOG_ERROR) << "LLVMExecutableModel::setValues for " << id << ", has initial assignment rule: " << symbols->hasInitialAssignmentRule(id);
-        }
 
         if (!result)
         {
