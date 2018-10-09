8909124ef31cae753ba06d39885be1277f2f7adc
Andy Somogyi <andy.somogyi@gmail.com>
2018-10-09 19:36:13 -0400

    updates to build system

---
+++ examples/dyn_linking/test.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <MxReactionNetwork.h>
+
+
+int main(int argv, const char** argc) {
+
+    // The basic way of creating a solver in C is:
+    // ISBMLSolver *solver = NULL;
+    ISBMLSolver *solver = NULL;
+
+    std::cout << "creating solver..." << std::endl;
+    HRESULT result = MxCreateSBMLSolver("/path/to/my/sbmlfile.sbml", NULL, &solver);
+
+    if(SUCCEEDED(result)) {
+        std::cout << "we’re good, made a solver, ready to use it" << std::endl;
+    }
+    else {
+        std::cout << "failed to make a solver, need to check the error code, figure out what happened" << std::endl;
+        return -1;
+    }
+
+    // grab the scalar system (the compiled SBML ode model) from the solver
+    IScalarSystem *system = 0;
+    result = solver->getScalarSystem(&system);
+    if(SUCCEEDED(result)) {
+        std::cout << "ready to use the scalar system, connect it to our compartment" << std::endl;
+    }
+    else {
+        std::cout << "this really should never happen" << std::endl;
+    }
+    return 0;
+}
