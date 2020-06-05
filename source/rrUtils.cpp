9e82e938499f55036229b9b8ccd80ea10fba9de0
Lucian Smith <lpsmith@uw.edu>
2020-06-05 14:46:15 -0700

    Warning fixes for rrUtils.

--- source/rrUtils.cpp
+++ source/rrUtils.cpp
@@ -326,7 +326,7 @@ vector<string> getLinesInFile(const string& fName)
     return lines;
 }
 
-int indexOf(const std::vector<std::string>& vec, const std::string& elem )
+ptrdiff_t indexOf(const std::vector<std::string>& vec, const std::string& elem )
 {
     if(!vec.size())
     {
@@ -334,7 +334,7 @@ int indexOf(const std::vector<std::string>& vec, const std::string& elem )
     }
     else
     {
-        int index = distance(vec.begin(), find(vec.begin(), vec.end(), elem));
+        ptrdiff_t index = distance(vec.begin(), find(vec.begin(), vec.end(), elem));
         return index;
     }
 }
@@ -345,7 +345,7 @@ string removeTrailingSeparator(const string& _folder, const char sep)
 {
     if((_folder.size() > 0) && (_folder[_folder.size() -1] == sep))
     {
-        const int endOfPathIndex = _folder.rfind(sep, _folder.size());
+        size_t endOfPathIndex = _folder.rfind(sep, _folder.size());
         string folder = _folder.substr(0, endOfPathIndex);
         return folder;
     }
@@ -670,7 +670,7 @@ string getWINAPIError(DWORD errorCode, LPTSTR lpszFunction)
     return errorMsg;
 }
 
-int populateFileSet(const string& folder, set<string>& files)
+size_t populateFileSet(const string& folder, set<string>& files)
 {
      //Get models file names in models folder
     string globPath =  rr::joinPath(folder, "*.xml");
