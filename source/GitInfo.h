1f7048812591fb4187d56da9dc4fe51a0017edc8
Ciaran Welsh <cwelsh2@uw.edu>
2020-08-04 00:11:32 +0100

    Commit before adding libsbml dependencies manually - the sbml dep package is playing up

--- source/GitInfo.h
+++ source/GitInfo.h
@@ -22,7 +22,7 @@ static const char* getGitBranch() {
 
 
 static const char* getGitLastCommit() {
-    return "3e137e2c56abeecd09b53249624aac14ae945761";
+    return "4e075bb2a366990224d50c6ff5a4635afe49c1bc";
 }
 
 #endif
