d999819d53e872b11be6e155282305ab3d523c02
Gary Geng <garygengxiao@gmail.com>
2020-08-14 16:39:22 -0500

    revert previous testing code

--- wrappers/C/Testing/main.cpp
+++ wrappers/C/Testing/main.cpp
@@ -118,15 +118,7 @@ int main(int argc, char* argv[])
 
         clog << "Running Suite SBML_l2v4\n";
         clog << "ModelPath " << gTSModelsPath << endl;
-        struct Only
-        {
-
-            bool operator()(const Test* const test) const
-            {
-                return test->m_details.testName == "966";
-            }
-        };
-        runner1.RunTestsIf(Test::GetTestList(), "SBML_TEST_SUITE", Only(), 0);
+        runner1.RunTestsIf(Test::GetTestList(), "SBML_TEST_SUITE", True(), 0);
     }
     if (args.Suites.find('F') != std::string::npos)
     {
