c0b706133c59bf05ff650c5e4e7336f65d8f1636
Debashish Roy <debashish.roy1998@gmail.com>
2020-08-23 02:01:41 +0530

    Lucian's change

--- rrplugins/scripts/install_tellurium_plugins.py
+++ rrplugins/scripts/install_tellurium_plugins.py
@@ -30,7 +30,7 @@ try:
     telPackageName = 'teplugins'
     spFolder = os.path.join(getSitePackagesFolder(), telPackageName)
     if DEBUG: 
-        print "spFolder " + spFolder
+        print ("spFolder " + spFolder)
     
     print('Installing Tellurium plugins to folder: ' + spFolder)   
     #clean destination folder   
@@ -41,15 +41,15 @@ try:
     #Figure what to copy        
     currentFolder = os.path.dirname(os.path.abspath(__file__))   
     if DEBUG: 
-        print "currentFolder " + currentFolder
+        print ("currentFolder " + currentFolder)
     
     pyFilesFolder = os.path.join(currentFolder, 'site-packages', telPackageName)    
     if DEBUG: 
-        print "pyFilesFolder " + pyFilesFolder
+        print("pyFilesFolder " + pyFilesFolder)
         
     pyFiles = glob.glob(os.path.join(pyFilesFolder, '*.py'))
     if DEBUG: 
-        print "pyFiles " + str(pyFiles)
+        print("pyFiles " + str(pyFiles))
         
     #Plugins 
     pluginsFolder = os.path.join(currentFolder,  'plugins')   
@@ -57,14 +57,14 @@ try:
                        
     #Copy the dlls that are in ..\bin folder
     #Binaries       
-    binFolder = os.path.join(currentFolder, 'bin')   
+    binFolder = os.path.join(currentFolder, 'bin')
     if DEBUG: 
-        print "binFolder " + binFolder
+        print("binFolder " + binFolder)
         
     bins = glob.glob(os.path.join(binFolder, '*.dll'))
     
     if DEBUG: 
-        print "bins " + str(bins)
+        print("bins " + str(bins))
                  
     rootFiles = pyFiles + bins
    
@@ -73,28 +73,27 @@ try:
                     
     #Copy root files
     for file in rootFiles:
-        print 'Copying file: ' + file + ' -> ' + spFolder 
+        print('Copying file: ' + file + ' -> ' + spFolder)
         shutil.copy(file, spFolder)
     
     #Copy plugins
     destPluginFolder = spFolder #Copy plugins into the same folder as the other dll's 
     if DEBUG: 
-        print "destPluginFolder " + destPluginFolder
+        print("destPluginFolder " + destPluginFolder)
     
     if not os.path.exists(destPluginFolder):
         os.makedirs(destPluginFolder)
             
     for file in plugins:
-        print 'Copying file: ' + file + ' -> ' + destPluginFolder
+        print('Copying file: ' + file + ' -> ' + destPluginFolder)
         shutil.copy(file, destPluginFolder)
     
     #Copy any files necessary for plugins to work properly
     pluginSupportFiles =['README.txt']    
     for file in pluginSupportFiles:
         theFile = os.path.join(pluginsFolder, file)
-        print 'Copying file: ' + theFile + ' -> ' + destPluginFolder
+        print('Copying file: ' + theFile + ' -> ' + destPluginFolder)
         shutil.copy(theFile, destPluginFolder)
 
 except Exception as e:
-    print 'There was a problem installing Tellurium plugins: ' + `e`
-
+    print('There was a problem installing Tellurium plugins: ' + e)
