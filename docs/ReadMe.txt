429635a18b8918c06978e8cc585e8fad6576a475
Ciaran Welsh <cwelsh2@uw.edu>
2020-10-12 20:02:45 +0100

    remove docs placeholder folder. continue modifying telplugins

--- docs/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-This directory contains a stub of a framework for the documentation.
-The idea is that we use "Breathe" to connect Doxygen to Sphinx. Then
-we just use sphinx.
-
-This will require some work. Contact Ciaran Welsh for help.
--- docs/docs/source/example_of_docs_source_folder/from_uri_python.py
+++
@@ -1,9 +0,0 @@
-from pyomexmeta import RDF
-
-# collect the link from "https://www.ebi.ac.uk/biomodels/BIOMD0000000308#Files"
-tyson2003 = "https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000308.2?filename=BIOMD0000000308_url.xml"
-
-# download the model, scan document for annotations and docs-build a graph
-rdf = RDF.from_uri(tyson2003, format="rdfxml")
-
-print(f"Number of annotations in graph: {len(rdf)}")
