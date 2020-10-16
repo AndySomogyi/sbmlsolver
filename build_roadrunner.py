a772da87eb9f4a73d677fcdfd12b45069b4d4fd8
Ciaran Welsh <cwelsh2@uw.edu>
2020-10-17 00:22:26 +0100

    Clean up some dead files and create azure pipelines yaml for CI

--- build_roadrunner.py
+++ build_roadrunner.py
@@ -1,12 +1,13 @@
-import os, sys
-import subprocess
 import argparse
+import os
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("dependencies", help="Path to install prefix of libroadrunner-deps", type=str)
 args = parser.parse_args()
 
-def do_check_call(commands:list, error_message=None):
+
+def do_check_call(commands: list, error_message=None):
     try:
         cmake_found = subprocess.check_call(commands)
     except subprocess.CalledProcessError as e:
@@ -15,10 +16,12 @@ def do_check_call(commands:list, error_message=None):
             print(error_message)
         exit(1)
 
+
 BASE_DIRECTORY = os.getcwd()
 
 # will error if cmake not available
-do_check_call(["cmake", "--version"], "Make sure cmake is available and your environment variables are correctly configured to allow the 'cmake' command to be run from shell")
+do_check_call(["cmake", "--version"],
+              "Make sure cmake is available and your environment variables are correctly configured to allow the 'cmake' command to be run from shell")
 
 # clone repo
 roadrunner_github = r"https://github.com/CiaranWelsh/roadrunner.git"
@@ -33,11 +36,12 @@ os.chdir(ROADRUNNER_BUILD_DIR)
 
 INSTALL_LOCATION = os.path.join(ROADRUNNER_DIR, "roadrunner-install")
 # cmake command
-do_check_call(["cmake", f"-DCMAKE_INSTALL_PREFIX={INSTALL_LOCATION}", f"-DRR_DEPENDENCIES_INSTALL_PREFIX={args['dependencies']}", ROADRUNNER_DIR])
+do_check_call(
+    ["cmake", f"-DCMAKE_INSTALL_PREFIX={INSTALL_LOCATION}", f"-DRR_DEPENDENCIES_INSTALL_PREFIX={args['dependencies']}",
+     ROADRUNNER_DIR])
 
 # build command
 do_check_call(["cmake", "--build", ROADRUNNER_BUILD_DIR, "-j", "12"])
 
 # install command
 do_check_call(["cmake" "--build", "install"])
-
