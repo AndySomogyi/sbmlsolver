7e53e1b60aa68f3bc115ef0443765509d7af8c7b
Ciaran Welsh <cwelsh2@uw.edu>
2021-07-22 18:52:47 +0100

    Implemented sorting algorithm for row and columns to be put in alphabetical order.

--- source/Matrix.h
+++ source/Matrix.h
@@ -6,6 +6,7 @@
 #define ROADRUNNER_MATRIX_H
 
 #include "rr-libstruct/lsMatrix.h"
+#include <algorithm>
 
 namespace rr {
 
@@ -24,25 +25,27 @@ namespace rr {
         using ls::Matrix<T>::setRowNames;
         using ls::Matrix<T>::rowNames;
         using ls::Matrix<T>::colNames;
+        using ls::Matrix<T>::swapCols;
+        using ls::Matrix<T>::swapRows;
 
         /**
          * @brief Constructor for creating a Matrix<T> from a
          * ls::Matrix<T>
          */
-         explicit Matrix<T>( ls::Matrix<T>& matrix){
-             resize(matrix.numRows(), matrix.numCols());
-             for (int i=0; i< numRows(); i++){
-                 for (int j=0; j<numCols(); j++){
-                     operator()(i, j) = matrix(i, j);
-                 }
-             }
-             // row and col names are references, so we make a copy
-             colNames = std::vector<std::string>(matrix.getColNames().begin(), matrix.getColNames().end());
-             rowNames = std::vector<std::string>(matrix.getRowNames().begin(), matrix.getRowNames().end());
-         }
+        explicit Matrix<T>(ls::Matrix<T> &matrix) {
+            resize(matrix.numRows(), matrix.numCols());
+            for (int i = 0; i < numRows(); i++) {
+                for (int j = 0; j < numCols(); j++) {
+                    operator()(i, j) = matrix(i, j);
+                }
+            }
+            // row and col names are references, so we make a copy
+            colNames = std::vector<std::string>(matrix.getColNames().begin(), matrix.getColNames().end());
+            rowNames = std::vector<std::string>(matrix.getRowNames().begin(), matrix.getRowNames().end());
+        }
 
-         explicit Matrix<T>( ls::Matrix<T>* matrix)
-                 : Matrix<T>(*matrix){}
+        explicit Matrix<T>(ls::Matrix<T> *matrix)
+                : Matrix<T>(*matrix) {}
 
         /**
          * @brief Element-wise equality operator to compare a Matrix<T> with
@@ -51,7 +54,7 @@ namespace rr {
          */
         bool operator==(Matrix<T> &other) {
             // if both are empty, result is true
-            if (empty() && other.empty()){
+            if (empty() && other.empty()) {
                 return true;
             }
             if (numRows() != other.numRows()) {
@@ -76,7 +79,7 @@ namespace rr {
         /**
          * @brief inequality operators
          */
-        bool operator!=( Matrix<T> &other) {
+        bool operator!=(Matrix<T> &other) {
             return !(*this == other);
         }
 
@@ -84,9 +87,9 @@ namespace rr {
          * @brief Element-wise compareison between this Matrix<double> with another @param other
          * Matrix<double>
          */
-        bool almostEquals( Matrix<double> &other, const double &tolerance) {
+        bool almostEquals(Matrix<double> &other, const double &tolerance) {
             // when both matrices are empty, they are equal
-            if (empty() && other.empty()){
+            if (empty() && other.empty()) {
                 return true;
             }
             if (numRows() != other.numRows()) {
@@ -107,9 +110,9 @@ namespace rr {
             return equals;
         }
 
-        Matrix<T> mult(Matrix<T> matrix){
+        Matrix<T> mult(Matrix<T> matrix) {
             ls::Matrix<T> mat = ls::mult(*this, matrix);
-            return Matrix<T>(mat);
+            return Matrix < T > (mat);
         }
 
         /**
@@ -120,6 +123,77 @@ namespace rr {
             if (getArray() == nullptr)
                 return true;
             return false;
+        }
+
+        /**
+         * @brief Reorder Matrix rows such that row names
+         * are in alphabetical order.
+         */
+        void sortRowsByLabels() {
+            if (rowNames.empty()) {
+                // don't sort unlabelled matrices
+                return;
+            }
+
+            // dont sort 0 and 1 length row labels
+            if (numRows() < 2) {
+                return;
+            }
+
+            std::vector<std::string> rowLabels = rowNames; // make copy
+
+            // now a cheeky bubble sort algorithm
+            // poor performance, but easy to implement
+            bool swapped = false;
+            for (int i = 0; i < rowLabels.size() - 1 ; i++) {
+                for (int j = 0; j < rowLabels.size() - 1; j++) {
+                    if (rowLabels[j] > rowLabels[j+1]) {
+                        swapRows(j, j+1);
+                        std::swap(rowLabels[j], rowLabels[j+1]);
+                        swapped = true;
+                    }
+                }
+                // little optimization
+                if (!swapped){
+                    break;
+                }
+            }
+            setRowNames(rowLabels);
+        }
+        /**
+         * @brief Reorder Matrix cols such that col names
+         * are in alphabetical order.
+         */
+        void sortColsByLabels() {
+            if (colNames.empty()) {
+                // don't sort unlabelled matrices
+                return;
+            }
+
+            // dont sort 0 and 1 length col labels
+            if (numCols() < 2) {
+                return;
+            }
+
+            std::vector<std::string> colLabels = colNames; // make copy
+
+            // now a cheeky bubble sort algorithm
+            // poor performance, but easy to implement
+            bool swapped = false;
+            for (int i = 0; i < colLabels.size() - 1 ; i++) {
+                for (int j = 0; j < colLabels.size() - 1; j++) {
+                    if (colLabels[j] > colLabels[j+1]) {
+                        swapCols(j, j+1);
+                        std::swap(colLabels[j], colLabels[j+1]);
+                        swapped = true;
+                    }
+                }
+                // little optimization
+                if (!swapped){
+                    break;
+                }
+            }
+            setColNames(colLabels);
         }
     };
 
