9093bb7239c9936e7697845c87ed7db00de37c0a
Ciaran Welsh <cw00137@gmail.com>
2021-09-16 23:33:19 +0100

    Add plotter for sensitivities. Add to docs

--- docs/source/Examples/sensitivities/sensitivities.py
+++ docs/source/Examples/sensitivities/sensitivities.py
@@ -1,6 +1,7 @@
+import roadrunner
 from roadrunner import RoadRunner
 from roadrunner.testing.TestModelFactory import TestModelFactory, getAvailableTestModels
-
+from os.path import join, dirname
 
 # note: use getAvailableTestModels() to see options for test models.
 sbml = TestModelFactory("OpenLinearFlux").str() # get the test model's sbml string
@@ -11,19 +12,14 @@ sbml = TestModelFactory("OpenLinearFlux").str() # get the test model's sbml stri
 model = RoadRunner(sbml)
 sensSolver = model.getSensitivitySolver()
 
-# start integration with sensitivities at 0, end at 10 and collect 11 data points
-# The return type is a 4-Tuple, a time vector and a 3D sensitivity matrix with
-# dimensions time x parameters x variables - that is, an array of matrices corresponding
-# to the time points in the time vector with parameters down the rows and model variables (species)
-# accross the columns. The third and forth elements are row and column names respectively.
 time, sens, rownames, colnames = model.timeSeriesSensitivities(0, 10, 11)
 
 print("time: ", time)
 print("rownames: ", rownames)
 print("colnames: ", colnames)
-
-# Note that in this model, the parameter kb is not used in any
-# rate law and therefore the sensitivities of model variables
-# to kb is always 0
 print(sens)
+
+fname = join(dirname(__file__), "OpenLinearFluxSensitivities.png")
+roadrunner.plotTimeSeriesSens(time, sens, rownames, colnames, ncol=2, fname=fname)
+print(f"Plot image saved to \"{fname}\"")
 
