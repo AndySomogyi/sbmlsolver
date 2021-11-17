fixed memory bug in performance test where antimomny holds on to too many models, consumed memory and then presumably slows future computation

test/PerformanceTests/NLinearChain.py
@@ -53,6 +53,8 @@ def generateNLinearModels(n: int):
         moduleName = f"linearChain{modelID}"
 
         if moduleName in antimony.getModuleNames():
+            # note this might not be needed now that
+            # we are freeing all models in every iteration
             yield antimony.getSBMLString(moduleName)
 
         else:
@@ -75,36 +77,9 @@ def generateNLinearModels(n: int):
                 raise ValueError("AntimonyError: " + antimony.getLastError())
 
             sbml = antimony.getSBMLString(f"linearChain{modelID}")
+            # clear memory associated with antimony
+            # or after a while it'll get clogged and we'll
+            # eventially run out of memory
+            antimony.clearPreviousLoads()
             yield sbml
 
-
-def doPlot(times: np.array, fname=os.path.join(os.path.dirname(__file__), "linearChainRoadRunnerOnly.png")):
-    fig = plt.figure()
-    plt.plot(range(N), times, "ro")
-    plt.xlabel("n species")
-    plt.ylabel("compile time (s)")
-    sns.despine(fig=fig)
-    plt.savefig(fname, dpi=150, bbox_inches='tight')
-
-
-if __name__ == "__main__":
-    # generate N models with $i \in range(N)$ species
-    N = 300
-
-    times = np.zeros((N,))
-    for i, sbmlString in enumerate(generateNLinearModels(N)):
-        start = time.time()
-        rrModel = RoadRunner(sbmlString)
-        times[i] = time.time() - start
-        i += 1
-        if i % 10 == 0:
-            print(i, sum(times[i - 10: i]))
-
-        if i == N - 1:
-            print(sbmlString)
-    total = times.sum()
-
-    print(times)
-    print(f"Took {total} seconds to build {N} models")
-
-    doPlot(times)

test/PerformanceTests/NReactionsUncoupled.py
@@ -61,6 +61,8 @@ def generateModelsWithNUncoupledReactions(n: int) -> str:
         moduleName = f"NReactions{int(np.floor((modelID / 2)))}"
         # if we've built this model before, it'll be in antimony's cache.
         if moduleName in antimony.getModuleNames():
+            # note this might not be needed now that
+            # we are freeing all models in every iteration
             yield antimony.getSBMLString(moduleName)
         else:
             modelString = f"model {moduleName}\n"
@@ -86,37 +88,14 @@ def generateModelsWithNUncoupledReactions(n: int) -> str:
 
             sbml = antimony.getSBMLString(f"NReactions{int(np.floor((modelID / 2)))}")
             yield sbml
+            # clear memory associated with antimony
+            # or after a while it'll get clogged and we'll
+            #eventially run out of memory
+            antimony.clearPreviousLoads()
             modelID += 2
             model_count += 1
-
-
-def doPlot(times: np.array, fname=os.path.join(os.path.dirname(__file__), "linearChainRoadRunnerOnly.png")):
-    fig = plt.figure()
-    plt.plot(range(len(times)), times, "ro")
-    plt.xlabel("n species")
-    plt.ylabel("compile time (s)")
-    sns.despine(fig=fig)
-    plt.savefig(fname, dpi=150, bbox_inches='tight')
 
 
 if __name__ == "__main__":
     # generate N models with $i \in range(N)$ uncoupled reactions
     N = 6
-
-    times = np.zeros((N,))
-    for i, sbmlString in enumerate(generateModelsWithNUncoupledReactions(N)):
-        start = time.time()
-        rrModel = RoadRunner(sbmlString)
-        times[i] = time.time() - start
-        i += 1
-        if (i % 10 == 0):
-            print(i, sum(times[i - 10: i]))
-
-        if i == N - 1:
-            print(sbmlString)
-    total = times.sum()
-
-    print(times)
-    print(f"Took {total} seconds to build {N} models")
-
-    doPlot(times)

test/PerformanceTests/performance_tests.py
@@ -70,6 +70,7 @@ def build_time(sbmlGeneratorFunction: callable, N: int, results_key: str, result
         rrModel = RoadRunner(sbmlString)
         times.append(time.time() - start)
         progressBar(i, N, results_key)
+        del sbmlString
     results[results_key] = times
     return times
 
@@ -92,6 +93,7 @@ def build_and_sim_time(sbmlGeneratorFunction: callable, N: int, results_key: str
         rrModel.simulate(0, 1000, 1001)
         times.append(time.time() - start)
         progressBar(i, N, results_key)
+        del sbmlString
     results[results_key] = times
     return times
 
@@ -117,6 +119,7 @@ def sim_time(sbmlGeneratorFunction: callable, N: int, results_key: str, results:
         rrModel.simulate(0, 1000, 1001)
         times.append(time.time() - start)
         progressBar(i, N, results_key)
+        del sbmlString
     results[results_key] = times
     return times
 
@@ -145,27 +148,27 @@ def plot(results: dict):
 
     def plot_n_reactions_build_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsUncoupledBuildTimes.png")
-        plotResults(times, fname, "n reactions (uncoupled)", "build time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "build time (s)", "NReactions (uncoupled)")
 
     def plot_n_reactions_build_and_sim_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsUncoupledBuildAndSimTimes.png")
-        plotResults(times, fname, "n reactions (uncoupled)", "build and sim time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "build and sim time (s)", "NReactions (uncoupled)")
 
     def plot_n_reactions_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsUncoupledSimTimes.png")
-        plotResults(times, fname, "n reactions (uncoupled)", "sim time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "sim time (s)", "NReactions (uncoupled)")
 
     def plot_n_reactions_increasing_connectivity_build_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsIncreasingConnectivityBuildTimes.png")
-        plotResults(times, fname, "n reactions (increasing connectivity)", "build time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "build time (s)", "NReactions (Increasing Connectivity)")
 
     def plot_n_reactions_increasing_connectivity_build_and_sim_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsIncreasingConnectivityBuildAndSimTimes.png")
-        plotResults(times, fname, "n reactions (increasing connectivity)", "build and sim time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "build and sim time (s)", "NReactions (Increasing Connectivity)")
 
     def plot_n_reactions_increasing_connectivity_time_results(times: np.ndarray):
         fname = os.path.join(os.path.dirname(__file__), "NReactionsIncreasingConnectivitySimTimes.png")
-        plotResults(times, fname, "n reactions (increasing connectivity)", "sim time (s)", "NReactions")
+        plotResults(times, fname, "n reactions", "sim time (s)", "NReactions (Increasing Connectivity)")
 
     plot_linear_chain_build_time_results(results["NLinearChain_BuildTime"])
     plot_linear_chain_build_and_sim_time_results(results["NLinearChain_BuildAndSimTime"])
@@ -201,7 +204,7 @@ if __name__ == "__main__":
 
     # The N parameter for the functions above.
     # Represents size of the problem to time.
-    N = 200
+    N = 600
 
     if USE_PICKLED_RESULTS and os.path.exists(RESULTS_PICKLE_FILE):
         with open(RESULTS_PICKLE_FILE, 'rb') as f: