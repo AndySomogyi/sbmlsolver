fa03a6f4bd2c6751114061966fbf8e1621a3bc7c
CiaranWelsh <cwelsh274@gmail.com>
2021-12-09 15:02:32 +0000

    Add thread-pool. Begin writing roadrunner container for parallel loading etc.

---
+++ source/rrRoadRunnerMap.h
@@ -0,0 +1,81 @@
+//
+// Created by Ciaran on 09/12/2021.
+//
+
+#ifndef ROADRUNNER_RRROADRUNNERMAP_H
+#define ROADRUNNER_RRROADRUNNERMAP_H
+
+#include "rrRoadRunner.h"
+#include <unordered_map>
+#include <thread>
+#include "thread_pool.hpp"
+
+namespace rr {
+
+    /**
+     * Unordered map for use with RoadRunnerMap. This type will do the 
+     * heavy lifting. Pointers to roadrunner models are stack allocated. 
+     * If this is insufficient, then we'll convert this to unique_ptr. 
+     */
+    using UnorderedMap = std::unordered_map<std::string, std::unique_ptr<RoadRunner>>;
+
+    class RoadRunnerMap {
+    public:
+        explicit RoadRunnerMap(const std::vector<std::string> &sbmlStringsOrFiles,
+                      unsigned int numProc = std::thread::hardware_concurrency() - 1);
+
+        std::vector<std::string> getKeys() const;
+
+        void insert(std::unique_ptr<RoadRunner> roadRunner);
+
+        void insert(const std::string &key, std::unique_ptr<RoadRunner> roadRunner);
+
+        void insert(const std::string &key, const std::string &sbmlOrFile);
+
+        void insert(const std::string &sbmlOrFile);
+
+        void remove(const std::string &key);
+
+        UnorderedMap::iterator begin() noexcept;
+
+        UnorderedMap::const_local_iterator cbegin() const noexcept;
+
+        UnorderedMap::iterator end() noexcept;
+
+        UnorderedMap::const_local_iterator cend() const noexcept;
+
+        bool empty() const;
+
+        unsigned int size() const;
+
+        void clear();
+
+        RoadRunner *operator[](const std::string &key);
+
+        RoadRunner *borrow(const std::string &key);
+
+        std::unique_ptr<RoadRunner> steal(const std::string &key);
+
+        size_t count(const std::string &key);
+
+        UnorderedMap::iterator find(const std::string &key);
+
+#if __cplusplus >= 202002L
+        bool contains(const std::string& key);
+#endif
+
+        unsigned int numProc ;
+
+    private:
+
+        void loadParallel(const std::vector<std::string> &sbmlFilesOrStrings);
+
+        std::mutex mutex;
+        UnorderedMap rrMap_{};
+        std::vector<std::string> keys_;
+        thread_pool pool;
+    };
+
+}
+
+#endif //ROADRUNNER_RRROADRUNNERMAP_H
---
+++ test/cxx_api_tests/RoadRunnerMapTests.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Ciaran on 09/12/2021.
+//
+
+#include "gtest/gtest.h"
+#include "rrRoadRunnerMap.h"
+#include "TestModelFactory.h"
+using namespace rr;
+
+class RoadRunnerMapTests : public ::testing::Test {
+public:
+    std::vector<std::string> sbml;
+    RoadRunnerMapTests() {
+        Logger::setLevel(Logger::LOG_DEBUG);
+        std::vector<std::string> input({
+            OpenLinearFlux().str(),
+            SimpleFlux().str(),
+            Brown2004().str(),
+            Venkatraman2010().str()
+        });
+        sbml.swap(input);
+    }
+};
+
+
+TEST_F(RoadRunnerMapTests, c){
+    RoadRunnerMap rrm(sbml);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
